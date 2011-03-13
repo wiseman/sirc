77e3999d921403db3c1830da4bbc780957ec9e22
wiseman <jjwiseman@gmail.com>
2011-03-12 21:33:06 -0800

    Now indexes local files or from S3.

--- sirc/log.py
+++ sirc/log.py
@@ -2,13 +2,14 @@ import datetime
 
 
 class Metadata():
-  def __init__(self, channel, date):
+  def __init__(self, path, channel, date):
+    self.path = path
     self.channel = channel
     self.date = date
+
   def __str__(self):
-    return '<log.Metadata channel=%s date=%s>' % (self.channel, self.date)
-#  def __repr__(self):
-#    return str(self)
+    return '<log.Metadata %s %r %s>' % (self.path, self.channel, self.date)
+
 
 def metadata_from_logpath(logpath):
   path_pieces = [p for p in logpath.split('/') if len(p) > 0]
@@ -20,7 +21,7 @@ def metadata_from_logpath(logpath):
   day_str = date_piece[6:8]
   
   date = datetime.date(int(year_str), int(month_str), int(day_str))
-  return Metadata(channel=channel_str, date=date)
+  return Metadata(path=logpath, channel=channel_str, date=date)
 
 def metadata_from_s3path(s3path):
   path_pieces = [p for p in s3path.split('/') if len(p) > 0]
@@ -31,5 +32,5 @@ def metadata_from_s3path(s3path):
   day_str = date_piece[3:5]
 
   date = datetime.date(int(year_str), int(month_str), int(day_str))
-  return Metadata(channel=channel_str, date=date)
+  return Metadata(path=s3path, channel=channel_str, date=date)
 
--- tools/index.py
+++ tools/index.py
@@ -11,100 +11,59 @@ import boto
 
 import sirc.util.s3
 import sirc.log
-
-
-TEMPLATE = Template('''
-<doc>
-  <field name="id">$id</field>
-  <field name="channel">$channel</field>
-  <field name="timestamp">$timestamp</field>
-  <field name="user">$user</field>
-  <field name="text">$message</field>
-  <field name="position">$position</field>
-</doc>
-''')
+import sirc.solr
 
 
 LOG_TIMESTAMP_HEADER_RE = re.compile(r'.*log: started (.+)/([0-9\.]+)')
+
 LOG_LINE_RE = re.compile(r'([0-9]+:[0-9]+:[0-9]+) <(\w+)> ?(.*)', re.UNICODE)
 
-def parse_log_line(line):
-  match = LOG_LINE_RE.match(line)
-  if match:
-    # timestamp, who, text
-    return match.groups()
+
+def _error(msg):
+  sys.stdout.flush()
+  sys.stderr.write('%s\n' % (msg,))
+
+def _usage():
+  sys.stdout.flush()
+  sys.stderr.write('Usage: %s <solr url> <logpath> [<logpath>...]\n' % (sys.argv[0],))
+  sys.stderr.write('Where <logpath> is a local file path to a log file or an s3:// url.\n')
 
 
-g_id_prefix = ''
-g_id_counter = 0
-
-def next_id():
-  global g_id_counter, g_id_prefix
-  g_id_counter += 1
-  return '%s:%s' % (g_id_prefix, g_id_counter)
-
-g_channel = None
-g_date_str = None
-
- 
-def xform_line(line, position):
-    global g_channel, g_date_str
-    result = parse_log_line(line)
-    if result:
-        timestamp, who, message = result
-        who = cgi.escape(recode(who))
-        message = recode(message)
-        if not has_ctrl(message):
-          message = cgi.escape(message)
-          timestamp = '%sT%sZ' % (g_date_str, timestamp,)
-          return TEMPLATE.substitute(id=next_id(),
-                                     channel=g_channel,
-                                     timestamp=timestamp,
-                                     user=who,
-                                     message=message,
-                                     position=position)
+def index_files(solr_url, paths):
+  for path in paths:
+    index_file(solr_url, path)
 
 
-def check_high_bit(path, text):
-  for char in text:
-    if ord(char) == 24:
-      raise Exception('Got char 24 in %s: %r' % (path, text))
-
-def has_ctrl(text):
-  for char in text:
-    if ord(char) < 32:
-      return True
-  return False
+def index_file(solr_url, path):
+  if path.startswith('s3://'):
+    index_s3_file(solr_url, path)
+  else:
+    index_local_file(solr_url, path)
 
 
-def xform_file(path):
-  global g_channel, g_date_str, g_id_prefix
+def index_s3_file(solr_url, path):
+  bucket, s3_path = parse_s3_url(path)
+  bucket = get_s3_bucket(bucket)
+  key = boto.s3.key.Key(bucket)
+  key.key = s3_path
+  log_sink = StringIO.StringIO()
+  log_data = sirc.log.metadata_from_s3path(s3_path)
+  log_contents = key.get_contents_as_string()
+  log_fp = StringIO.StringIO(log_contents)
+  return index_fp(solr_url, log_data, log_fp)
+
+
+def index_local_file(solr_url, path):
   with open(path, 'rb') as f:
-    # Extract the channel name and date from the first line of the
-    # log.
-    first_line = f.readline()
-    match = LOG_TIMESTAMP_HEADER_RE.match(first_line)
-    if not match:
-      raise Exception('Unable to parse log header %s: %r' % (path, first_line))
-    g_channel = match.groups()[0]
-    date_str = match.groups()[1]
-    g_id_prefix = '%s/%s' % (g_channel, date_str)
-    year = 2000 + int(date_str[0:2])
-    month = int(date_str[3:5])
-    day = int(date_str[6:8])
-    g_date_str = '%s-%02d-%02d' % (year, month, day)
-
-    position = f.tell()
-    line = f.readline()
-    while line != '':
-      xformed = xform_line(line, position)
-      position = f.tell()
-      line = f.readline()
-      if xformed:
-        print xformed
+    log_data = sirc.log.metadata_from_logpath(path)
+    index_fp(solr_url, log_data, f)
 
 
-def index_fp(log_data, fp):
+def index_fp(solr_url, log_data, fp):
+  post_records(solr_url, list(index_records_for_fp(log_data, fp)))
+
+
+def index_records_for_fp(log_data, fp):
   logging.info('Indexing %s' % (log_data,))
   first_line = fp.readline()
   match = LOG_TIMESTAMP_HEADER_RE.match(first_line)
@@ -113,26 +72,72 @@ def index_fp(log_data, fp):
   assert log_data.channel == match.groups()[0]
   
   position = fp.tell()
+  line_num = 0
   line = fp.readline()
   while line != '':
-    xformed = xform_line(line, position)
+    xformed = index_record_for_line(log_data, line, line_num, position)
     position = fp.tell()
+    line_num += 1
     line = fp.readline()
     if xformed:
-      print xformed
-  
-  
+      yield xformed
+
+
+def post_records(solr_url, index_records):
+  conn = sirc.solr.SolrConnection(url=solr_url)
+  conn.add_many(index_records)
+  conn.commit()
+
+def index_record_for_line(log_data, line, line_num, position):
+  result = parse_log_line(line)
+  if result:
+    timestamp, who, message = result
+    who = cgi.escape(recode(who))
+    message = recode(message)
+    message = cgi.escape(message)
+    date_str = '%s-%02d-%02d' % (log_data.date.year,
+                                 log_data.date.month,
+                                 log_data.date.day)
+    timestamp = '%sT%sZ' % (date_str, timestamp,)
+
+    return {
+      'id': id,
+      'channel': log_data.channel,
+      'timestamp': timestamp,
+      'user': who,
+      'text': message,
+      'position': position
+      }
+
+
+def parse_log_line(line):
+  match = LOG_LINE_RE.match(line)
+  if match:
+    # timestamp, who, text
+    return match.groups()
+
+
+import unicodedata
+
+def is_ctrl_char(c):
+  return unicodedata.category(c) == 'Cc'
+
 
 def recode(text):
-  recoded_text = unicode(text, 'cp1252', 'replace')
-  recoded_text = recoded_text.encode('utf8', 'xmlcharrefreplace')
+  recoded_text = unicode(text, 'cp1252', 'strict')
+  recoded_text = ''.join([c for c in recoded_text if not is_ctrl_char(c)])
   return recoded_text
 
 
-def index_local_file(path):
-  with open(path, 'rb') as f:
-    log_data = sirc.log.metadata_from_logpath(path)
-    index_from_fp(log_data, f)
+
+
+# ------------------------------------------------------------
+# S3 utils
+# ------------------------------------------------------------
+
+def parse_s3_url(url):
+  pieces = [p for p in url.split('/') if len(p) > 0]
+  return pieces[1], '/'.join(pieces[2:])
 
 
 g_connection = None
@@ -157,28 +162,10 @@ def get_s3_bucket(bucket_name):
     bucket = g_buckets[bucket]
   return bucket
 
-  
-def index_s3_file(solr_url, path):
-  bucket, s3_path = parse_s3_url(path)
-  bucket = get_s3_bucket(bucket)
-  key = boto.s3.key.Key(bucket)
-  key.key = s3_path
-  log_sink = StringIO.StringIO()
-  log_data = sirc.log.metadata_from_s3path(s3_path)
-  log_contents = key.get_contents_as_string()
-  log_fp = StringIO.StringIO(log_contents)
-  return index_fp(log_data, log_fp)
 
-def index_file(solr_url, path):
-  if path.startswith('s3://'):
-    index_s3_file(solr_url, path)
-  else:
-    index_local_file(solr_url, path)
-
-
-def index_files(solr_url, paths):
-  for path in paths:
-    index_file(solr_url, path)
+# ------------------------------------------------------------
+# Main
+# ------------------------------------------------------------
 
 def main(argv):
   logging.basicConfig(level=logging.INFO)
@@ -190,21 +177,7 @@ def main(argv):
   files = args[1:]
   index_files(solr_url, files)
 
-def _error(msg):
-  sys.stdout.flush()
-  sys.stderr.write('%s\n' % (msg,))
-
-def _usage():
-  sys.stdout.flush()
-  sys.stderr.write('Usage: %s <solr url> <logpath> [<logpath>...]\n' % (sys.argv[0],))
-  sys.stderr.write('Where <logpath> is a local file path to a log file or an s3:// url.\n')
   
-
-
-def parse_s3_url(url):
-  pieces = [p for p in url.split('/') if len(p) > 0]
-  return pieces[1], '/'.join(pieces[2:])
-
 if __name__ == '__main__':
   main(sys.argv)
 
