aa406f3dc80a8a9cdb96abded472f83a631cca71
wiseman <jjwiseman@gmail.com>
2011-03-24 20:47:32 -0700

    Starting HTMLification of logs.

---
+++ tools/render.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from __future__ import with_statement
+import sys
+import time
+import logging
+
+import sirc.logrender
+
+
+
+
+def main(args):
+  for path in args[1:]:
+    with open(path, 'rb') as file:
+      log_text = file.read()
+      time_start = time.time()
+      html = sirc.logrender.render_log(log_text)
+      time_end = time.time()
+      print html
+      logging.info('Rendered %s in %s ms',
+                   path,
+                   int((time_end - time_start) * 1000))
+
+
+if __name__ == '__main__':
+  main(sys.argv)
+  
