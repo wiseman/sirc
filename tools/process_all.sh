b5a9332de8da72f248f896b9133805a1d670a6b6
John Wiseman <jjwiseman@gmail.com>
2011-06-11 17:52:03 -0700

    A new useful script.

---
+++ tools/process_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SOLR_URL=$1
+
+(
+    cd ~/src/irc-logs
+    env/bin/python tools/nefget.py
+)
+
+time (
+    cd ~/src/sirc
+    PYTHONPATH=.:../irc-logs/tools env/bin/python tools/index.py $SOLR_URL ~/src/irc-logs/logs/freenode/emacs/*/[0-9]*
+    #PYTHONPATH=.:../irc-logs/tools env/bin/python tools/upload.py ~/src/irc-logs/logs/*/*/lisp/*[0-9]
+)
