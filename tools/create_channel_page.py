31eb18ff7b51fae6622c4ba5b33aa911071d18f3
wiseman <jjwiseman@gmail.com>
2011-07-06 03:06:50 -0700

    Tweak HTML

--- tools/create_channel_page.py
+++ tools/create_channel_page.py
@@ -4,16 +4,15 @@ import sys
 
 import ircloglib
 
+import sirc.browse
 
 
-NUM_DAYS_BY_MONTH = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-
-# { (server, channel):
-#    { year:
-#        {month #: ((day, # msgs), ...)}}}
+# {(server, channel):
+#    {year:
+#        {month #: {day: # msgs}}}}
 
 def get_channels_stats(log_dir):
+  activity_counts = []
   stats = {}
   for log_data in ircloglib.all_logs(log_dir):
     #print log_data.path
@@ -22,34 +21,37 @@ def get_channels_stats(log_dir):
     month_stats = year_stats.get(log_data.start_time.month, {})
     activity_count = get_activity_count(log_data.path)
     month_stats[log_data.start_time.day] = activity_count
+    activity_counts.append(activity_count)
     year_stats[log_data.start_time.month] = month_stats
     channel_stats[log_data.start_time.year] = year_stats
     stats[(log_data.server, log_data.channel)] = channel_stats
-  return stats
+  return stats, sorted(activity_counts)
 
-def create_channel_page(server, channel, channel_stats):
-  out = StringIO.StringIO()
-  out.write('<h1>%s/%s</h1>\n' % (server, channel))
-  for year in sorted(channel_stats.keys(), reverse=True):
-    out.write('<h2>%s</h2>\n' % (year,))
-    out.write('<table>\n')
-    for month in sorted(channel_stats[year].keys(), reverse=True):
-      out.write('<tr><td><big>%s<big></td>' % (calendar.month_name[month],))
-      for day in range(NUM_DAYS_BY_MONTH[month]):
-        day += 1
-        if day in channel_stats[year][month]:
-          out.write('<td align="center">%s</td>' % (day,))
-        else:
-          out.write('<td align="center">--</td>')
-      out.write('</tr>\n')
-    out.write('</table>\n')
-  return out.getvalue()
+
+
 
 def get_activity_count(path):
   with open(path, 'rb') as f:
     return len(f.readlines())
 
+
 if __name__ == '__main__':
-  stats = get_channels_stats(sys.argv[1])
+  print """
+<html>
+<head>
+<style type="text/css">
+  .activity-xlow   { background-color: rgb(  0,   0, 255); }
+  .activity-low    { background-color: rgb(  0, 102, 255); }
+  .activity-medium { background-color: rgb(  0, 255, 255); }
+  .activity-high   { background-color: rgb(255, 255,   0); }
+  .activity-xhigh  { background-color: rgb(255, 101,   0); }
+</style>
+<title>Browsing</title>
+</head>
+<body>
+"""
+  stats, activity_counts = get_channels_stats(sys.argv[1])
+  print '<!-- %s -->' % (activity_counts,)
   for server, channel in sorted(stats.keys()):
-    print create_channel_page(server, channel, stats[(server, channel)])
+    print sirc.browse.get_channel_browse_html(server, channel, stats[(server, channel)], activity_counts)
+  print "</body></html>"
