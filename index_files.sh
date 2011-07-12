d291d8d021ddba25313dffe7255961eb71667b6c
John Wiseman <jjwiseman@gmail.com>
2011-07-11 19:11:34 -0700

    Replaced by tools/index.py.

--- index_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PYTHONPATH=. env/bin/python tools/index.py $*
-
-if [ $? -ne 0 ]; then
-    exit 255
-fi
--- index_multi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-NUM_THREADS=5
-SOLR_URL=$1
-shift
-
-echo $* | xargs -P $NUM_THREADS -n 30 ./index_files.sh $SOLR_URL
